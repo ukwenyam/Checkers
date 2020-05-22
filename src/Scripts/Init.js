27d84ffb812b38f4a05bb771da29383a8db8ff82
Michael <45051389+ukwenyam@users.noreply.github.com>
2020-05-22 00:22:34 -0500

    chat window added

--- src/Scripts/Init.js
+++ src/Scripts/Init.js
@@ -1,5 +1,14 @@
 import { writable } from 'svelte/store';
 import { User } from './User.js';
+import io from 'socket.io-client'
+
+
+const socket = io('http://localhost:4000');
+
+console.log("cresting socket");
+console.log(socket);
+
+export const currSocket = writable(socket);
 
 export const currUser = writable(new User());
 
